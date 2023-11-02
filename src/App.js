f33da41c8e276b6b0f2f3cc0f45c4a98f0c5cb66
faridalieff <faridalieff@AlieffsMacBook-Pro.local>
2023-11-03 02:09:36 +0400

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <code>src/App.js</code>-u redaktə edin və yenidən yükləmək üçün yadda saxlayın.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/FaridAlieff/first-react-app/tree/main"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          0pen Github project <code>FaridAlieff</code>
         </a>
       </header>
     </div>
